445d9e023ac56c60f0e8b8bd1db53d5020f0af16
gemy26 <102263771+gemy26@users.noreply.github.com>
2024-04-19 22:07:20 +0200

    Time: 32 ms (56.82%) | Memory: 21.5 MB (30.64%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+bool ok(vector<int> &piles, long long  mid, int h){
+    long long  houres = 0;
+    for(auto i : piles){
+        houres += ( (i / mid) + (i % mid == 0 ? 0 : 1) );
+    }
+    return houres <= h;
+}
+    int minEatingSpeed(vector<int>& piles, int h) {
+    long long  l = 1, r = 1e12, ans = -1;
+     while(r >= l){
+        long long  mid =  ( l + r ) / 2 ;
+        int sum = 0;
+        if(ok(piles,mid,h)){
+            r = mid - 1;
+            ans = mid;
+        }
+        else{
+            l = mid + 1;
+        }
+     }
+     return ans;   
+    }
+};
