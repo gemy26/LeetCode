e3f4f04d85da90789bfc825fbeb4e769885b1719
gemy26 <102263771+gemy26@users.noreply.github.com>
2024-10-08 22:13:53 +0300

    Time: 3 ms (45.33%) | Memory: 8 MB (36.34%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res = "";
+        for(int i = 0; i < max(word1.size() ,word2.size()); i++){
+            if(i < word1.size()) res +=  word1[i];
+            if(i < word2.size()) res +=  word2[i];
+        }
+        return res;
+    }
+};
