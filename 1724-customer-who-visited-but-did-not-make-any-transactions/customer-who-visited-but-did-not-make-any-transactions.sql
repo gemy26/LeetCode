65a1ffc3a4649fe1b3c0e00aa00e7071dbc08bf5
gemy26 <102263771+gemy26@users.noreply.github.com>
2024-10-23 03:15:51 +0300

    Time: 1403 ms (57.27%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,4 +1,6 @@
-/* Write your T-SQL query statement below */
-select v.customer_id, count(v.visit_id) as count_no_trans from visits v  left join  Transactions t on v.visit_id = t.visit_id 
+select v.customer_id, count(v.visit_id) as count_no_trans 
+from visits v  
+left join  Transactions t 
+on v.visit_id = t.visit_id 
 where t.transaction_id is NULL
 group by(v.customer_id)
