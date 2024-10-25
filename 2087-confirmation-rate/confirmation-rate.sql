8f3f3b42b5049a940fffaf8a9b742ab10857ce09
gemy26 <102263771+gemy26@users.noreply.github.com>
2024-10-25 15:46:18 +0300

    Time: 533 ms (49.66%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,12 +1,11 @@
 SELECT 
-    T.user_id, 
+    s.user_id, 
     ROUND(
-    CAST(COUNT(CASE WHEN T.action = 'confirmed' THEN 1 END) AS DECIMAL) / 
+    CAST(COUNT(CASE WHEN c.action = 'confirmed' THEN 1 END) AS DECIMAL) / 
     CAST(COUNT(*) AS DECIMAL) , 2)
     AS confirmation_rate
-FROM (
-    SELECT s.user_id AS user_id, c.action AS action 
+
     FROM Confirmations c 
     RIGHT JOIN Signups s ON s.user_id = c.user_id
-) T
-GROUP BY T.user_id;
+
+GROUP BY s.user_id;
