56a4906d7e1527d1a617812e2c937f56b79585e9
gemy26 <102263771+gemy26@users.noreply.github.com>
2024-11-04 22:39:29 +0200

    Time: 50 ms (29.53%) | Memory: 29.8 MB (57.27%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        string comp = "";
+        int n = (int) word.size();
+        for (int i = 0; i < n;) {
+            int j = i + 1;
+            while (j < n && word[j] == word[i] && j - i + 1 <= 9) {
+                j++;
+            }
+            comp += to_string(j - i);
+            comp += word[i];
+            i = j;
+        }
+        return comp;
+    }
+};
