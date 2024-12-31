1d3767f75ce6b5f2c6783126650ce66f7b66048d
gemy26 <102263771+gemy26@users.noreply.github.com>
2024-12-31 22:00:36 +0200

    Time: 3 ms (26.55%) | Memory: 14.3 MB (14.36%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-   int calc(int idx, vector <int> &dp, vector <int> &days, vector <int> &costs){
-    if(idx >= days.size())
-        return 0;
-    int &ret = dp[idx];
-    if(~ret)
-        return ret;
-    ret = 1e9;
-    ret = min({
-        costs[0] + calc(idx + 1, dp, days, costs),
-        costs[1] + calc(lower_bound(days.begin(), days.end(), days[idx] + 7) - days.begin(), dp, days, costs),
-        costs[2] + calc(lower_bound(days.begin(), days.end(), days[idx] + 30) - days.begin(), dp, days, costs),
-    });
-    return ret;
-}
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        int n = days.size();
-        vector <int> dp(n, -1);
-        int ans = calc(0, dp, days, costs); //index, day, vector
-        return ans;
+        sort(days.begin(), days.end());
+        vector <int> dp(days.size(), -1);
+        return solve(0, days, costs, dp);
+    }
+
+    int pos(vector<int>& days, int i, int plus) {
+        return  ( upper_bound(days.begin(), days.end(), days[i] + plus) - days.begin() );
+    }
+
+    int solve(int i, vector<int>& days, vector<int>& costs, vector <int> &dp) {
+        if(i >= days.size()) return 0;
+        int &ret = dp[i];
+        if(~ret) return ret;
+        ret = INT_MAX;
+
+        ret = min({
+            solve(pos(days, i, 29), days, costs, dp) + costs[2],
+            solve(pos(days, i, 6), days, costs, dp) + costs[1],
+            solve(pos(days, i, 0), days, costs, dp) + costs[0]
+        });
+
+        return dp[i] = ret;
     }
 };
