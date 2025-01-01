81c518b155c0da0487c210bbc0fb761c851a52b6
gemy26 <102263771+gemy26@users.noreply.github.com>
2025-01-01 15:10:22 +0200

    Time: 36 ms (5.34%) | Memory: 8.7 MB (38.26%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -5,7 +5,7 @@ public:
         for(int i = 0; i < s.size(); i++){
             ones_cnt += s[i] == '1';
         }
-        for(int i = 0; i + 1< s.size(); i++){
+        for(int i = 0; i + 1 < s.size(); i++){
             zeros_cnt += s[i] == '0';
             ones_cnt -= s[i] == '1';
             cout << ones_cnt <<" " << zeros_cnt <<  endl;
