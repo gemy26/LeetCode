1225ea67f1a3d0d70a85d61e576c9cd3f1307c07
gemy26 <102263771+gemy26@users.noreply.github.com>
2025-01-02 15:11:08 +0200

    Time: 191 ms (6.23%) | Memory: 69.4 MB (70.64%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
-        vector <int> prefix(words.size(), 0);
-        vector <int> ans (queries.size());
-        cout << "here 1\n";
-        for(int i = 0; i < words.size(); i++){
-            if(isVowel(words[i][0]) && isVowel(words[i][words[i].size()-1])){
-                prefix[i] ++;
+    vector<int> vowelStrings(vector<string>& words,
+                             vector<vector<int>>& queries) {
+        vector<int> prefix(words.size(), 0);
+        vector<int> ans(queries.size());
+        for (int i = 0; i < words.size(); i++) {
+            if (isVowel(words[i][0]) &&
+                isVowel(words[i][words[i].size() - 1])) {
+                prefix[i]++;
             }
-            if(i){
+            if (i) {
                 prefix[i] += prefix[i - 1];
             }
         }
 
-        cout << "here 2\n";
-
-        for(int i = 0; i < queries.size(); i ++){
+        for (int i = 0; i < queries.size(); i++) {
             cout << i << " ";
-            ans[i] = prefix[queries[i][1]] - ( queries[i][0] - 1 >= 0 ? prefix[queries[i][0] - 1] : 0 );
+            ans[i] = prefix[queries[i][1]] -
+                     (queries[i][0] - 1 >= 0 ? prefix[queries[i][0] - 1] : 0);
         }
 
         return ans;
     }
-    bool isVowel(char ch){
+    bool isVowel(char ch) {
         return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
     }
 };
