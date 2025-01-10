d8d76f3b97a3c28bac92ed26df6667979b64f33e
gemy26 <102263771+gemy26@users.noreply.github.com>
2025-01-10 15:37:23 +0200

    Time: 57 ms (58.90%) | Memory: 114.6 MB (29.22%) - LeetSync

--- 952-word-subsets/word-subsets.cpp
+++ 952-word-subsets/word-subsets.cpp
@@ -4,21 +4,21 @@ public:
         vector<string> ans;
         vector<vector<int>> frq_a((int)a.size(), vector<int>(30, 0));
         vector<int> frq_b(30, 0), frq((int)a.size(), 0);
+
         for (int i = 0; i < (int)a.size(); i++) {
             for (auto ch : a[i]) {
-                frq_a[i][ch - 'a'] ++;
+                frq_a[i][ch - 'a']++;
             }
         }
 
         for (int i = 0; i < (int)b.size(); i++) {
-            vector <int> tmp_frq(30, 0);
+            vector<int> tmp_frq(30, 0);
             for (auto ch : b[i]) {
-                tmp_frq[ch - 'a'] ++;
+                tmp_frq[ch - 'a']++;
             }
-            for (char c = 'a'; c <= 'z'; c++){
+            for (char c = 'a'; c <= 'z'; c++) {
                 frq_b[c - 'a'] = max(frq_b[c - 'a'], tmp_frq[c - 'a']);
             }
-
         }
 
         for (int j = 0; j < (int)a.size(); j++) {
