ff406e3a308c177b7d0bb0b2d10869e5fa0337cc
gemy26 <102263771+gemy26@users.noreply.github.com>
2025-01-16 02:18:35 +0200

    Time: 131 ms (5.28%), Space: 88.9 MB (5.28%) - LeetHub

---
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int xorAllNums(vector<int>& a, vector<int>& b) {
+        map<int, long long > mp;
+        int ans = 0;
+
+        for (auto i : a) {
+            mp[i] += b.size();
+        }
+
+        for (auto i : b) {
+            mp[i] += (int) a.size();
+        }
+
+        for (auto& [key, val] : mp) {
+            if(val&1){
+                ans ^= key;
+            }
+        }
+
+        return ans;
+    }
+};
