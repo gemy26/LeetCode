7aedb0174ead69556a5dad618c0871ab0b83d25f
gemy26 <102263771+gemy26@users.noreply.github.com>
2025-01-25 14:37:12 +0200

    Time: 386 ms (16.21%), Space: 359.6 MB (5.4%) - LeetHub

---
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        unordered_map<int, deque<int>> mp;
+        unordered_map<int, int> groups;
+        vector<int> ans(nums.size());
+        vector <int> original = nums;
+        sort(nums.begin(), nums.end());
+        int cnt = 0;
+        mp[cnt].push_back(nums[0]);
+        groups[nums[0]] = cnt;
+        for (int i = 1; i < nums.size(); i++) {
+            if (abs(nums[i] - nums[i - 1]) > limit) {
+                cnt++;
+            }
+            mp[cnt].push_back(nums[i]);
+            groups[nums[i]] = cnt;
+        }
+        
+        for (int i = 0; i < nums.size(); i++) {
+            ans[i] = mp[groups[original[i]]].front();
+            mp[groups[original[i]]].pop_front();
+        }
+        return ans;
+    }
+};
