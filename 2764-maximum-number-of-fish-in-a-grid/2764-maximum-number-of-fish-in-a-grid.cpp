ee8620d214af662d506040b98d1ad44345691c32
gemy26 <102263771+gemy26@users.noreply.github.com>
2025-01-28 03:50:16 +0200

    Time: 16 ms (36.08%), Space: 96.7 MB (48.16%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector <vector <bool>> vis;
+    vector<vector<bool>> vis;
     int dx[4] = {1, -1, 0, 0};
     int dy[4] = {0, 0, 1, -1};
     int N, M;
@@ -10,15 +10,17 @@ public:
         if (!isValid(i, j) || vis[i][j] || grid[i][j] == 0)
             return 0;
         vis[i][j] = true;
-        return grid[i][j] + rec(i + 1, j, grid) + rec(i - 1, j, grid) + rec(i, j - 1, grid) + rec(i, j + 1, grid);
+        return grid[i][j] + rec(i + 1, j, grid) + rec(i - 1, j, grid) +
+               rec(i, j - 1, grid) + rec(i, j + 1, grid);
     }
     int findMaxFish(vector<vector<int>>& grid) {
         N = grid.size(), M = grid[0].size();
+        vis = vector<vector<bool>>(N, vector<bool>(M, false));
         int ans = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (grid[i][j] > 0) {
-                    vis = vector <vector <bool>>(N, vector <bool>(M, false));
+                if (grid[i][j] > 0 && !vis[i][j]) {
+                    //vis = vector<vector<bool>>(N, vector<bool>(M, false));
                     ans = max(ans, rec(i, j, grid));
                 }
             }
