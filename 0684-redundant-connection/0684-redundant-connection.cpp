4c3a595eb5ebd16a93a00f481a46ad78b5df67a7
gemy26 <102263771+gemy26@users.noreply.github.com>
2025-01-29 23:50:36 +0200

    Time: 0 ms (100%), Space: 13.1 MB (55.83%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    class DisjointSets {
+    private:
+	    vector<int> parents;
+	    vector<int> sizes;
+
+    public:
+	    DisjointSets(int size) : parents(size), sizes(size, 1) {
+		for (int i = 0; i < size; i++) { parents[i] = i; }
+	    }
+
+	/** @return the "representative" node in x's component */
+	    int find(int x) { return parents[x] == x ? x : (parents[x] = find(parents[x])); }
+
+	/** @return whether the merge changed connectivity */
+	    bool unite(int x, int y) {
+		int x_root = find(x);
+		int y_root = find(y);
+		if (x_root == y_root) { return false; }
+
+		if (sizes[x_root] < sizes[y_root]) { swap(x_root, y_root); }
+		sizes[x_root] += sizes[y_root];
+		parents[y_root] = x_root;
+		return true;
+	}
+
+	/** @return whether x and y are in the same connected component */
+	    bool connected(int x, int y) { return find(x) == find(y); }
+};
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector <int> ans;
+        DisjointSets dsu(edges.size() + 1);
+        for(auto edge : edges){
+            auto[u, v] = make_pair(edge[0], edge[1]);
+            if(dsu.connected(u, v)){
+                ans.push_back(u);
+                ans.push_back(v);
+            }
+            
+            dsu.unite(u, v);
+        }
+        return ans;
+    }
+};
