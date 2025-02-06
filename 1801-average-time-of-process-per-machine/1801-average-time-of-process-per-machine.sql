500570234e62e850f876d3b436a4492a18d90d70
gemy26 <102263771+gemy26@users.noreply.github.com>
2025-02-06 02:07:19 +0200

    Time: 313 ms (85.38%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
 SELECT A.machine_id, Round(AVG(ABS(A.timestamp - B.timestamp)), 3) AS processing_time
-FROM Activity A join Activity B ON A.machine_id = B.machine_id 
+FROM Activity as A join Activity as B ON A.machine_id = B.machine_id 
 where A.process_id = B.process_id and A.activity_type  = 'start' and B.activity_type = 'end' 
 Group By A.machine_id;
