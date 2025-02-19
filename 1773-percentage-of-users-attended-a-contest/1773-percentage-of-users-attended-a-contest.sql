39592047cb6cbe03618f71d64ddf0887a712808b
gemy26 <102263771+gemy26@users.noreply.github.com>
2025-02-19 19:31:30 +0200

    Time: 1926 ms (35.94%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -1,19 +1,6 @@
-WITH TotalUsers AS (
-    SELECT COUNT(*) * 1.0 AS total_users FROM Users
-),
-UserRegistrations AS (
-    SELECT contest_id, user_id 
-    FROM Register 
-    GROUP BY contest_id, user_id
-),
-ContestParticipation AS (
-    SELECT contest_id, COUNT(user_id) AS registered_users
-    FROM UserRegistrations
-    GROUP BY contest_id
-)
-SELECT 
-    CP.contest_id, 
-    ROUND((CP.registered_users * 100.0) / TU.total_users, 2) AS percentage
-FROM ContestParticipation CP
-CROSS JOIN TotalUsers TU
-ORDER BY percentage DESC, CP.contest_id ASC;
+/* Write your T-SQL query statement below */
+SELECT R.contest_id, 
+ROUND((COUNT(DISTINCT R.user_id)*100.00 / (SELECT COUNT(user_id) FROM USERS)), 2) as percentage
+FROM Register as R 
+Group By R.contest_id
+Order By percentage DESC, R.contest_id ASC
