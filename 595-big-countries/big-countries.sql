200ebe4c0d9eaa71024a8f44636fbefbbd27350d
ahmed gamal <102263771+gemy26@users.noreply.github.com>
2025-04-21 10:41:45 +0200

    Time: 261 ms (90.60%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,2 +1,2 @@
--- Write your PostgreSQL query statement below
-select name, population, area from world where area >= 3000000 or population >= 25000000 
+# Write your MySQL query statement below
+select name, population, area from world where area >= 3000000 or population >= 25000000
