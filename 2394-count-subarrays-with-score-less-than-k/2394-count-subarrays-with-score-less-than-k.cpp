e2ef9ccb1098ebb5d1c052262e210327d9fb8506
ahmed gamal <102263771+gemy26@users.noreply.github.com>
2025-04-28 22:53:20 +0300

    Time: 45 ms (5.25%), Space: 108.2 MB (6.72%) - LeetHub

--- 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.cpp
+++ 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, long long k) {
-        int l = 0, r = 0, n = nums.size();
-        long long ans = 0, sum = 0;
-        while(r < n){
-            if(sum * (r - l) < k){
-                sum += nums[r ++];
+        int n = nums.size();
+        long long ans = 0;
+        auto v = vector <long long> (nums.begin(), nums.end());
+        for (int i = 1; i < n; i++) {
+            v[i] += v[i - 1] ;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int l = i, r = n - 1, f = -1, mid;
+            while (r >= l) {
+                mid = l + (r - l) / 2;
+                long long  range = mid - i + 1;
+                long long sum = v[mid] - (i ? v[i - 1] : 0);
+                //cout << i << " " << mid << " " << range <<" "<< sum << endl;
+                if (range * sum < k) {
+                    l = mid + 1;
+                    f = mid;
+                } else {
+                    r = mid - 1;
+                }
             }
-            while(r >= l && sum * (r - l) >= k){
-                sum -= nums[l ++];
+            if (~ f) {
+                ans += (f - i + 1);
             }
-            ans += (r - l);
         }
         return ans;
     }
