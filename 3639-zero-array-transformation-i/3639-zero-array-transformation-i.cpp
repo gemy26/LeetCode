b3481bfd01710cb29477df98ed465eadb4e995b4
ahmed gamal <102263771+gemy26@users.noreply.github.com>
2025-05-21 02:56:53 +0300

    Time: 4 ms (83.42%), Space: 293 MB (51.3%) - LeetHub

---
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& q) {
+        vector<int> a(nums.size() + 2, 0);
+        for (int i = 0; i < q.size(); i++) {
+            int l = q[i][0], r = q[i][1];
+            a[l] -= 1;
+            a[r + 1] += 1;
+        }
+        for (int i = 1; i < a.size(); i++) {
+            a[i] += a[i - 1];
+        }
+        bool ans = true;
+        for (int i = 0; i < nums.size(); i++) {
+           // cout << a[i] << " " << nums[i] << endl;
+            ans &= (max(a[i] + nums[i], 0) == 0);
+        }
+        return ans;
+    }
+};
