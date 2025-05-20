15a19dbd090c00dd6e2687b7720b56d1bba87260
ahmed gamal <102263771+gemy26@users.noreply.github.com>
2025-05-21 02:58:33 +0300

    Time: 3 ms (87.07%), Space: 292.7 MB (86.84%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -4,15 +4,15 @@ public:
         vector<int> a(nums.size() + 2, 0);
         for (int i = 0; i < q.size(); i++) {
             int l = q[i][0], r = q[i][1];
-            a[l] -= 1;
-            a[r + 1] += 1;
+            a[l] += 1;
+            a[r + 1] -= 1;
         }
         for (int i = 1; i < a.size(); i++) {
             a[i] += a[i - 1];
         }
         bool ans = true;
         for (int i = 0; i < nums.size(); i++) {
-            ans &= (abs(a[i]) >= nums[i]);
+            ans &= (a[i] >= nums[i]);
         }
         return ans;
     }
