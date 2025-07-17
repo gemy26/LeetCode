bc3caacf6645e70c4777e833193b6536f28c620a
ahmed gamal <102263771+gemy26@users.noreply.github.com>
2025-07-17 23:16:45 +0300

    Time: 2589 ms (5.08%) | Memory: 90.4 MB (55.81%) - LeetSync

--- 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.cpp
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
-    int maximumLength(vector<int>& v, int k) {
-        int n = (int) v.size();
-        vector <vector <int>> dp(n + 1, vector <int>(k + 1, 1));
-        int ans = 0;
-        for(int i = 0; i < n ; i ++){
-            for(int j = i + 1; j < n ; j ++){
-                int mod = (v[i] + v[j]) % k;
-                dp[j][mod] = max(dp[j][mod], 1 + dp[i][mod]);
-                ans = max(ans, dp[j][mod]);
+    int solve(int prevMod, int curr, int n, int k, vector<int> &nums, vector<vector<int>>& dp) {
+        if (curr >= n - 1) return 0;
+
+        if (dp[curr][prevMod + 1] != -1) return dp[curr][prevMod + 1];
+
+        int res = 0;
+
+        if (prevMod == -1) {
+            res = solve(prevMod, curr + 1, n, k, nums, dp);
+
+            for (int j = curr + 1; j < n; ++j) {
+                int mod = (nums[curr] + nums[j]) % k;
+                res = max(res, 2 + solve(mod, j, n, k, nums, dp));
+            }
+        } else {
+            for (int j = curr + 1; j < n; ++j) {
+                if ((nums[curr] + nums[j]) % k == prevMod) {
+                    res = max(res, 1 + solve(prevMod, j, n, k, nums, dp));
+                    break; 
+                }
             }
         }
 
-        return ans;
+        return dp[curr][prevMod + 1] = res;
+    }
+
+    int maximumLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(k + 2, -1)); 
+        return solve(-1, 0, n, k, nums, dp);
     }
 };
