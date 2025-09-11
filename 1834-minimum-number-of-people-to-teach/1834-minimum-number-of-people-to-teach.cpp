6b37eff822bfcaedbd7e9067578e30f68d0424b3
ahmed gamal <102263771+gemy26@users.noreply.github.com>
2025-09-11 04:26:34 +0300

    Time: 860 ms (5.88%), Space: 242.2 MB (5.88%) - LeetHub

---
+++ 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int minimumTeachings(int n, vector<vector<int>>& languages,
+                         vector<vector<int>>& friendships) {
+
+        map<int, set<int>> mp, knows;
+        set<int> noConn;
+        int ans = 1e9;
+
+        for (int i = 0; i < languages.size(); i++) {
+            for (auto j : languages[i]) {
+                mp[j].insert(i + 1);
+                knows[i + 1].insert(j);
+            }
+        }
+
+        // get all relationships that cannot communicate with each others and
+        // brute force over the languages and count the minimum
+
+        for (auto friendship : friendships) {
+            int u = friendship[0], v = friendship[1];
+            bool can = false;
+            for (auto lang : languages[u - 1]) {
+                if (knows[v].count(lang)) {
+                    can = true;
+                    break;
+                }
+            }
+            if (!can) {
+                noConn.insert(v);
+                noConn.insert(u);
+            }
+        }
+
+        for (int lang = 1; lang <= n; lang++) {
+            int cnt = 0;
+            for (auto u : noConn) {
+                if (knows[u].count(lang) == 0) {
+                    cnt++;
+                }
+            }
+            ans = min(cnt, ans);
+        }
+
+        return ans;
+    }
+};
