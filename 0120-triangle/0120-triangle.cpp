08fba9e894e65e9572ac9fd29f53c31f1862e995
ahmed gamal <102263771+gemy26@users.noreply.github.com>
2025-09-26 03:28:46 +0300

    Time: 0 ms (100%), Space: 13.5 MB (22.1%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -5,20 +5,18 @@ public:
         vector<vector<int>> dp(n);
 
         for (int i = 0; i < n; ++i) {
-            dp[i].resize(i + 1, 0);
+            dp[i].resize(i + 1, INT_MAX);
         }
 
         dp[0][0] = triangle[0][0];
 
-        for (int i = 1; i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
             for (int j = 0; j <= i; ++j) {
-                if (j == 0) {
-                    dp[i][j] = dp[i - 1][j] + triangle[i][j];
-                } else if (j == i) {
-                    dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
-                } else {
-                    dp[i][j] =
-                        min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+                if(i + 1 < n){
+                    dp[i + 1][j] = min(triangle[i + 1][j] + dp[i][j], dp[i + 1][j]);
+                }
+                if(i + 1 < n && j + 1 < triangle[i + 1].size()){
+                    dp[i + 1][j + 1] = min(triangle[i + 1][j + 1] + dp[i][j], dp[i + 1][j + 1]);
                 }
             }
         }
