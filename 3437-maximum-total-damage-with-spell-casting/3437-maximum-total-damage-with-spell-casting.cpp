b28dbf233460e6ab297d6bc617a7727b51b1c96f
ahmed gamal <102263771+gemy26@users.noreply.github.com>
2025-10-12 03:51:32 +0300

    Time: 498 ms (16.35%), Space: 244.1 MB (15.66%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
-    long long solve(int idx, vector<pair<long long,long long>> &v, vector<long long> &dp)
-    {
-        if(idx>=v.size()){
-            return 0;
-        }
-        
-        if(dp[idx]!=-1) return dp[idx];
 
-        //skip current
-        long long skip = solve(idx+1,v,dp);
+    long long calc(int idx, vector<pair<long long,long long>> &v, vector<long long> &dp){
+         if (idx >= v.size())
+                return 0;
 
-        //if we consider the current one -> we need to decide what will be the next valid idx
-        int j = idx+1;
-        while(j<v.size() && v[j].first<=v[idx].first+2) j++;
+            long long& ret = dp[idx];
+            if (~ret)
+                return ret;
 
-        long long consider = v[idx].second + solve(j,v,dp);
+            long long op2 = calc(idx + 1, v, dp);
 
-        return dp[idx]=max(skip,consider);
+            int pos = lower_bound(
+                v.begin(), v.end(),
+                make_pair(v[idx].first + 3, 0LL)) - v.begin();
+
+            long long op1 = v[idx].second + calc(pos, v, dp);
+
+            return ret = max(op1, op2);
     }
+
     long long maximumTotalDamage(vector<int>& power) {
-        int n = power.size();
-        map<long long,long long> mp;
-        for(int i=0;i<n;i++)
-        {
-            mp[power[i]]+=power[i];
+        map<long long, long long> mp;
+        for (int x : power)
+            mp[x] += x;
+
+        vector<pair<long long, long long>> v;
+        for (auto it : mp) {
+            v.push_back({it.first, it.second});
         }
-        vector<pair<long long,long long>> v;
-        for(auto it:mp){
-            v.push_back({it.first,it.second});
-        }
-        sort(v.begin(),v.end());
-        vector<long long> dp(v.size(), -1);
-        return solve(0,v,dp);
+        sort(v.begin(), v.end());
+
+        int n = (int)v.size();
+
+        vector<long long> dp(n, -1);
+
+        return calc(0, v, dp);
     }
 };
