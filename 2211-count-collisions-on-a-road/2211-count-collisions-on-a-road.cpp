0d5b075ca611d3ae672bb8687c6a0b3af9619dcf
ahmed gamal <102263771+gemy26@users.noreply.github.com>
2025-12-04 23:43:35 +0200

    Time: 15 ms (55.85%), Space: 19.1 MB (83.63%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     int countCollisions(string directions) {
-        int ans = 0, R = -1;
+        int ans = 0, R = 0;
         bool coll = 0;
         int n = directions.size();
         for (int i = 0; i < n; i++) {
+            // cout << i << " " << R << " " << coll << " ";
             if (directions[i] == 'L') {
-                if (R >= 0) {
+                if (R > 0) {
                     ans += R + 1;
                     R = 0;
+                    coll = 1;
+                } else if(coll){
+                    ans ++;
                 }
             } else if (directions[i] == 'S') {
-                if (R > 0) {
-                    ans += R;
-                }
-                 R = 0;
-            } else if (directions[i] == 'R') {
                 if (R >= 0) {
-                    R += 1;
-                } else{
-                    R = 1;
+                    ans += R;
+                    R = 0;
                 }
+                 coll = 1;
+            } else if (directions[i] == 'R') {
+               R ++;
+               coll = 0;
             }
+            // cout << ans << endl;
         }
         return ans;
     }
