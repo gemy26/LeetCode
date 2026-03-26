e23a11319e049e70fc350158c4672d24e6d3acc0
ahmed gamal <ahmed.gamal.5511@icloud.com>
2026-03-26 20:01:47 +0200

    Time: 0 ms (100%), Space: 52.4 MB (25.13%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        vector<bool> dp(n, false);
-        dp[0] = true;
-
-        for(int i = 0; i < n; i++) {
-            if(!dp[i]) continue;  // can't reach i, skip
-            for(int j = 1; j <= nums[i]; j++) {
-                if(i + j >= n) break;
-                dp[i + j] = true;
-            }
-        }
-
-        return dp[n - 1];
+       int maxReach = 0;
+       for(int i = 0; i < nums.size(); i ++){
+        if(i > maxReach) return false;
+        maxReach = max(maxReach, i + nums[i]);
+       }
+       return true;
     }
 };
