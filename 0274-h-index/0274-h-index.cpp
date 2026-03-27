9bb67106e8fde6b768b9a3883c5cb408ca722905
ahmed gamal <ahmed.gamal.5511@icloud.com>
2026-03-27 09:10:36 +0200

    Time: 0 ms (100%), Space: 12.3 MB (65.97%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -4,11 +4,11 @@ public:
         // citations[i] <= n - i => maximize
         int ans = 0;
         int n = c.size();
-        sort(c.rbegin(), c.rend());
+        sort(c.begin(), c.end());
         for(int i = 0; i < n; i ++){
             // cout << i + 1  << "  " << c[i] << endl;
-            if(c[i] >= i + 1){
-                ans = i + 1;
+            if(c[i] >= n - i){
+                ans = max(ans, n - i);
             }
         }
         return ans;
