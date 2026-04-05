04ae0f02ae4cfc8e6bc15bd3c014b22a804e1f16
ahmed gamal <ahmed.gamal.5511@icloud.com>
2026-04-05 03:12:03 +0200

    Time: 43 ms (84.91%), Space: 29.3 MB (28.94%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,18 +3,23 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        set<vector<int>> s;
         vector<vector<int>> ans;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                auto it = lower_bound(nums.begin() + j + 1, nums.end(),
-                                      -(nums[i] + nums[j]));
-                if (it != nums.end() && *it + nums[i] + nums[j] == 0) {
-                    s.insert({nums[i], nums[j], *it});
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int j = n - 1, k = i + 1;
+            while (j > k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0) {
+                    k ++;
+                } else if (sum > 0) {
+                    j --;
+                } else {
+                    ans.push_back({nums[i], nums[k], nums[j]});
+                    k ++;
+                    while(nums[k] == nums[k - 1] && k < j) k ++;
                 }
             }
         }
-        for(auto i : s) ans.push_back(i);
         return ans;
     }
 };
