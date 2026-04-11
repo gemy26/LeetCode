3cc2fd77a0fe26b583872788f2d85840c07c2abc
ahmed gamal <ahmed.gamal.5511@icloud.com>
2026-04-11 18:48:03 +0200

    Time: 29 ms (53.01%), Space: 109.1 MB (87.57%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     int shortestSubarray(vector<int>& nums, int k) {
-        // use min heap to get ride of min prefix and keep yout current sum
-        // greater than or equal current state
-        priority_queue<pair<long, int>, vector<pair<long, int>>, greater<>> pq;
-        pq.push({0, -1});
-        long long l = 0, r = 0, ans = INT_MAX, sum = 0, prefix = 0;
-        while (r < nums.size()) {
-            prefix += nums[r];
-            while (!pq.empty() && prefix - pq.top().first >= k) {
-                ans = min(ans, r - pq.top().second);
-                pq.pop();
+        int n = nums.size();
+        vector<long> prefix(n + 1, 0);
+        for (int i = 0; i < n; ++i) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+
+        deque<int> dq;
+        int ans = INT_MAX;
+
+        for (int i = 0; i <= n; i++) {
+            if (prefix[i] >= k)
+                ans = min(ans, i + 1);
+            while (!dq.empty() && prefix[i] - prefix[dq.front()] >= k) {
+                ans = min(ans, i - dq.front());
+                dq.pop_front();
             }
-            pq.push({prefix, r});
-            r++;
+            while (!dq.empty() && prefix[i] <= prefix[dq.back()]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
         }
         return ans == INT_MAX ? -1 : ans;
     }
