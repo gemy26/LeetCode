ed34883aba4098616c88367ce6980f9a4ea0848a
ahmed gamal <ahmed.gamal.5511@icloud.com>
2026-04-16 21:16:03 +0200

    Time: 0 ms (100%), Space: 72.9 MB (43.97%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector <int> prefix(n + 2, 0), ans(n);
+        for(int i = 0; i < bookings.size(); i ++){
+            int x = bookings[i][0], y = bookings[i][1], delta = bookings[i][2];
+            prefix[x] += delta;
+            prefix[y + 1] -= delta;
+        }
+        for(int i = 1; i <= n; i ++){
+            prefix[i] += prefix[i - 1];
+        }
+        for(int i = 1; i <= n; i ++){
+            ans[i - 1] = prefix[i];
+        }
+        return ans;
+    }
+};
