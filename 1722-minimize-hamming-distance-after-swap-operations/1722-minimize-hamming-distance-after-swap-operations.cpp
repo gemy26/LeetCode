ac5c6180a10eec036bae5c4cd805017ae246d6f5
ahmed gamal <ahmed.gamal.5511@icloud.com>
2026-04-21 07:09:45 +0200

    Time: 187 ms (38.22%), Space: 144.4 MB (81.67%) - LeetHub

---
+++ 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector <int> par, sz;
+    int find(int x){
+        if(par[x] == x) return x;
+        return par[x] = find(par[x]);
+    }
+    void join(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a == b) return;
+        if(sz[a] > sz[b]) swap(a, b); //a is the smaller
+        par[a] = b;
+        sz[b] += sz[a]; 
+    }
+    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
+        int n = source.size();
+        par = vector <int> (n + 1, 0);
+        sz = vector <int> (n + 1, 0);
+        for(int i = 0; i < n; i ++){
+            par[i] = i;
+            sz[i] = 1;
+        }
+
+        for(auto pair : allowedSwaps){
+            join(pair[0], pair[1]);
+        }
+
+        map<int, map<int, int>> mp;
+        for(int i = 0; i < n; i ++){
+            int par = find(i);
+            mp[par][source[i]] ++;
+        }
+        int ans = 0;
+        for(int i = 0; i < n; i ++){
+            if(mp[find(i)][target[i]] > 0){
+                mp[find(i)][target[i]] --;
+            } else{
+                ans ++;
+            }
+        }
+        return ans;
+    }
+};
