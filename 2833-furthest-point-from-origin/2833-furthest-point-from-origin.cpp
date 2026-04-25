921a501976b8974b3469fa83ce6191f31af8d547
ahmed gamal <ahmed.gamal.5511@icloud.com>
2026-04-25 23:46:52 +0300

    Time: 0 ms (100%), Space: 9.2 MB (28.4%) - LeetHub

---
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int R = 0, L = 0, free = 0;
+        int n = moves.size();
+        for(auto i : moves){
+            R += i == 'R';
+            L += i == 'L';
+            free += i == '_';
+        }
+        if(R > L){
+            swap(R, L);
+        }
+        int diff = L - R;
+        return n - (L + R) + diff;
+    }
+};
