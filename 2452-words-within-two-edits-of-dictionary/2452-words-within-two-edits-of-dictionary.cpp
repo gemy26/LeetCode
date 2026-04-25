a64f2de4dea119a5390248c21e5bba4f31a8b3cd
ahmed gamal <ahmed.gamal.5511@icloud.com>
2026-04-26 01:28:12 +0300

    Time: 7 ms (50.82%), Space: 34.5 MB (5.12%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -1,20 +1,53 @@
+struct TrieNode{
+    TrieNode *child[26];
+    bool isEnd;
+    TrieNode(){
+        memset(child, 0, sizeof(child));
+        isEnd = false;
+    }
+};
 class Solution {
 public:
+    TrieNode* root = new TrieNode();
+    void insert(string &word){
+        TrieNode *node = root;
+        for(char c : word){
+            int idx = c - 'a';
+            if(!node->child[idx]) node->child[idx] = new TrieNode();
+            node = node->child[idx];
+        }
+        node->isEnd = true;
+    }
+
+    bool dfs(string &word, int i, TrieNode* node, int cnt){
+        if(!node) return false;
+        if(cnt > 2) return false;
+        if(i == word.size()){
+            return node->isEnd;
+        }
+        int idx = word[i] - 'a';
+        if(node->child[idx]){
+            if(dfs(word, i + 1, node->child[idx], cnt)){
+                return true;
+            }
+        }
+        if(cnt < 2){
+            for(int c= 0; c < 26; c ++){
+                if(c == idx) continue;
+                if(node->child[c]){
+                    if(dfs(word, i + 1, node->child[c], cnt + 1)) return true;
+                }
+            }
+        }
+        return false;
+    }
+
     vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        for (auto& w : dictionary) insert(w);
         vector <string> ans;
-        vector <bool> taken(queries.size(), false);
-        for(int i = 0; i < queries.size(); i ++){
-            for(auto j : dictionary){
-                int diff = 0;
-                for(int k = 0; k < j.size(); k ++){
-                    if(j[k] != queries[i][k]) {
-                        diff ++;
-                    }
-                }
-                if(diff <= 2 && !taken[i]){
-                    ans.push_back(queries[i]);
-                    taken[i] = true;
-                }
+        for(auto& i : queries){
+            if(dfs(i, 0, root, 0)){
+                ans.push_back(i);
             }
         }
         return ans;
