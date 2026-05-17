8859291a79f6dc7b64a30ed9905dcc483d1cde26
ahmed gamal <ahmed.gamal.5511@icloud.com>
2026-05-17 15:39:19 +0300

    Time: 0 ms (100%), Space: 102.2 MB (79.84%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -4,35 +4,15 @@ public:
         int n1 = nums1.size();
         int n2 = nums2.size();
 
-        reverse(nums1.begin(), nums1.end());
-        reverse(nums2.begin(), nums2.end());
-
-        int ans = 0;
-
-        for (int ri = 0; ri < n1; ri++) {
-
-            int l = 0, r = n2 - 1;
-            int best = -1;
-
-            while (l <= r) {
-                int mid = (l + r) / 2;
-
-                if (nums2[mid] >= nums1[ri]) {
-                    best = mid;
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
-                }
-            }
-
-            if (best != -1) {
-                int orig_i = n1 - 1 - ri;
-                int orig_j = n2 - 1 - best;
-
-                if (orig_i <= orig_j) {
-                    ans = max(ans, orig_j - orig_i);
-                }
-            }
+        int ans = 0, i = 0, j = 0;
+        while (i < n1 && j < n2) {
+            if (j >= i && nums1[i] <= nums2[j]) {
+                ans = max(j - i, ans);
+                j ++;
+            } else if (j >= i)
+                i ++;
+            else
+                j ++;
         }
 
         return ans;
