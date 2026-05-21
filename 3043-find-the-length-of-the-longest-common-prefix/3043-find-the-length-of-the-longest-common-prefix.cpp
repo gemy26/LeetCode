e8b3716fc43e07b8ea6bf3e4a6e154537d7d8da4
ahmed gamal <ahmed.gamal.5511@icloud.com>
2026-05-21 18:01:11 +0300

    Time: 134 ms (80.17%), Space: 160.7 MB (43.5%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,27 +1,55 @@
+class TrieNode {
+public:
+    TrieNode* children[10];
+    TrieNode() {
+        for (int i = 0; i < 10; ++i) {
+            children[i] = nullptr;
+        }
+    }
+};
+
+class Trie {
+public:
+    TrieNode* root;
+    Trie() { root = new TrieNode(); }
+    void insert(int num) {
+        TrieNode* curr_node = root;
+        string s = to_string(num);
+        for (char dig : s) {
+            if (!curr_node->children[dig - '0']) {
+                curr_node->children[dig - '0'] = new TrieNode();
+            }
+            curr_node = curr_node->children[dig - '0'];
+        }
+    }
+
+    int findLongestPrefix(int num) {
+        TrieNode* node = root;
+        string s = to_string(num);
+        int len = 0;
+        for (char dig : s) {
+            if (node->children[dig - '0']) {
+                len++;
+                node = node->children[dig - '0'];
+            } else {
+                break;
+            }
+        }
+        return len;
+    }
+};
+
 class Solution {
 public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        Trie trie;
         int ans = 0;
-        map <int, bool> vis;
-        for(auto i : arr1){
-            int num = i;
-            while (num > 0){
-                vis[num] = true;
-                num /= 10;
-            }
+        for (auto i : arr1) {
+            trie.insert(i);
         }
-
-        for(auto i : arr2){
-            int num = i;
-            while (num > 0){
-                if(vis.find(num) != vis.end()){
-                    string s = to_string(num);
-                    ans = max(ans, (int)s.size());
-                }
-                num /= 10;
-            }
+        for (int i : arr2) {
+            ans = max(ans, trie.findLongestPrefix(i));
         }
-
         return ans;
     }
 };
