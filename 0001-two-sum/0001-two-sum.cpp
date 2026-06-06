26eca50fa86bdbbedec3db1263f93e2327424057
ahmed gamal <ahmed.gamal.5511@icloud.com>
2026-06-06 22:30:50 +0300

    Time: 35 ms (34.57%), Space: 11.1 MB (99.99%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       vector<int>v;
+ map<int,int>mp;
+       for(int i =0 ;i < nums.size();i++){
+           int x = target-nums[i];
+           if(mp.find(x) != mp.end()){
+               v.push_back(mp[x]), v.push_back(i); break;
+           }
+           else
+           {
+               mp.insert({nums[i],i});
+           }
+       }
+       return v;
+    }
+
+    
+};
